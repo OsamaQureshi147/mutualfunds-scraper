6752ed8e885fff4c02fd45ef53da723b08f0da58
Osama Ehsan Qureshi <osamahsan@gmail.com>
2023-06-20 13:07:07 +0500

    completed fund categories scraping

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
 const { scrapeFundTypes } = require("./scraper-functions/scrapeFundTypes");
 const { scrapeAMCs } = require("./scraper-functions/scrapeAMCs");
+const {
+  scrapeFundCategories,
+} = require("./scraper-functions/scrapeFundCategories");
+
 const { websiteUrl } = require("./constants");
 
 const startScraping = async () => {
@@ -10,8 +14,10 @@ const startScraping = async () => {
   await page.goto(websiteUrl);
   const fundTypesTable = await scrapeFundTypes({ page });
   const amcsTable = await scrapeAMCs({ page });
+  const fundCategories = await scrapeFundCategories({ page });
+  await browser.close();
   await fs.writeFile("fund-types.txt", JSON.stringify(fundTypesTable));
   await fs.writeFile("amcs.txt", JSON.stringify(amcsTable));
-  await browser.close();
+  await fs.writeFile("fund-categories.txt", JSON.stringify(fundCategories));
 };
 startScraping();
