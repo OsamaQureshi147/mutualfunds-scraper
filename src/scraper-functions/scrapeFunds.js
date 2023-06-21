e9d7e1120083684438d8be0fa24eb2225d345649
Osama Ehsan Qureshi <osamahsan@gmail.com>
2023-06-21 15:03:43 +0500

    Gets filtered rows but with amcs

--- src/scraper-functions/scrapeFunds.js
+++ src/scraper-functions/scrapeFunds.js
@@ -5,36 +5,56 @@ const scrapeFunds = ({ page }) => {
     (async () => {
       try {
         const fundType = await page.$eval("#sellink", (el) => el.textContent);
-        const fundsTable = await page.$$eval(
-          "table.mydata tr.border",
-          (rows) => {
-            const tabelHeaders = [
-              "fundName",
-              "category",
-              "inceptionDate",
-              "aum",
-            ];
-            return rows.map((row) => {
-              // return the rows as JSON with key-value pairs
-              let fundRecordObj = {};
-              fundRecordObj["uid"] = row.id;
-              const cells = Array.from(row.querySelectorAll("td"));
-              cells.forEach((cell, i) => {
-                fundRecordObj[tabelHeaders[i]] = cell.textContent.trim();
-              });
-              return fundRecordObj;
+        const fundsTable = await page.$$eval("table.mydata tr", (rows) => {
+          // Need to filter the rows based on classLists.
+          // The tr with "border" class consists of funds data
+          // The tr with no class consists of the AMC which is required to develop the relation
+          const fundsAndAmcsRows = rows.filter(
+            (row) =>
+              Object.values(row.classList).includes("border") ||
+              !Object.values(row.classList).length
+          );
+
+          // let amcsWithIndexes = [];
+
+          // fundsAndAmcsRows.forEach((row, i) => {
+          //   if (!Object.values(row.classList).length) {
+          //     const amcName = row.querySelector("td").textContent.trim();
+          //     amcsWithIndexes.push({ name: amcName, index: i });
+          //   }
+          // });
+
+          const tableHeaders = [
+            "fundName",
+            "category",
+            "inceptionDate",
+            "aum",
+            "amc",
+          ];
+
+          return fundsAndAmcsRows.map((row) => {
+            // return the rows as JSON with key-value pairs
+            let fundRecordObj = {};
+            fundRecordObj["uid"] = row.id;
+            fundRecordObj["amc"] = "dummyAMC";
+            const cells = Array.from(row.querySelectorAll("td"));
+            cells.forEach((cell, i) => {
+              fundRecordObj[tableHeaders[i]] = cell.textContent.trim();
             });
-          }
-        );
-        const formattedFundsTable = fundsTable.map((fundRecord) => {
-          return {
-            ...fundRecord,
-            fundName: parseAbbreviation(fundRecord.fundName),
-            category: parseAbbreviation(fundRecord.category),
-            fundType: parseAbbreviation(fundType),
-          };
+            // return Object.values(row.classList);
+            return fundRecordObj;
+          });
         });
-        res(formattedFundsTable);
+        console.log("Rows,", fundsTable);
+        // const formattedFundsTable = fundsTable.map((fundRecord) => {
+        //   return {
+        //     ...fundRecord,
+        //     fundName: parseAbbreviation(fundRecord.fundName),
+        //     category: parseAbbreviation(fundRecord.category),
+        //     fundType: parseAbbreviation(fundType),
+        //   };
+        // });
+        res(fundsTable);
       } catch (error) {
         rej("Error scraping funds");
       }
