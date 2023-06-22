e6dae8f2284889e1428456ce6a7ec4f013057d78
Osama Ehsan Qureshi <osamahsan@gmail.com>
2023-06-22 14:25:40 +0500

    Adds all page scraping for amcs

--- src/server.js
+++ src/server.js
@@ -13,26 +13,24 @@ const startScraping = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(websiteUrl);
-  const fundTypesTable = await scrapeFundTypes({ page });
 
-  let amcsTable = [];
-  for (const fundType of fundTypesTable) {
-    const { link } = fundType;
-    if (link !== websiteUrl) {
-      const page = await browser.newPage();
-      await page.goto(link);
-      amcsTable = [...amcsTable, ...(await scrapeAMCs({ page }))];
-    }
-  }
+  const fundTypesTable = await scrapeFundTypes({ page });
+  const amcsTable = await scrapeAMCs({
+    page,
+    browser,
+    links: fundTypesTable?.map(({ link }) => link),
+  });
+  const fundCategoriesTable = await scrapeFundCategories({ page });
+  const fundsTable = await scrapeFunds({ page });
+
   await browser.close();
-  console.log("fundTypesTable", fundTypesTable);
-  console.log("amcsTable", amcsTable);
+
   await fs.writeFile("fund-types.txt", JSON.stringify(fundTypesTable));
   await fs.writeFile("amcs.txt", JSON.stringify(amcsTable));
-  // await fs.writeFile(
-  //   "fund-categories.txt",
-  //   JSON.stringify(fundCategoriesTable)
-  // );
-  // await fs.writeFile("funds.txt", JSON.stringify(fundsTable));
+  await fs.writeFile(
+    "fund-categories.txt",
+    JSON.stringify(fundCategoriesTable)
+  );
+  await fs.writeFile("funds.txt", JSON.stringify(fundsTable));
 };
 startScraping();
