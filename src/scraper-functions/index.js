e403ec853893ab99be721ed7a35a3640168815bb
Osama Ehsan Qureshi <osamahsan@gmail.com>
2023-06-26 11:09:45 +0500

    Adds abstraction before adding mutual funds

--- src/scraper-functions/index.js
+++ src/scraper-functions/index.js
@@ -1,31 +1,11 @@
-const { scrapeFunds } = require("./funds/scrapeFunds");
-const { scrapeAMCs } = require("./scrapeAMCs");
-const { scrapeFundCategories } = require("./scrapeFundCategories");
 const { scrapeFundTypes } = require("./scrapeFundTypes");
-
-const scrapeCommonEntities = async ({ page, browser }) => {
-  const fundTypesTable = await scrapeFundTypes({ page });
-
-  const amcsTablePromise = scrapeAMCs({
-    page,
-    browser,
-    linksToScrape: fundTypesTable?.map(({ link }) => link),
-  });
-
-  const fundCategoriesTablePromise = scrapeFundCategories({
-    page,
-    browser,
-    linksToScrape: fundTypesTable?.map(({ link }) => link),
-  });
-
-  const [amcsTable, fundCategoriesTable] = await Promise.all([
-    amcsTablePromise,
-    fundCategoriesTablePromise,
-  ]);
-  return { fundTypesTable, amcsTable, fundCategoriesTable };
-};
+const { scrapeCommonEntities } = require("./scrapeCommonEntities");
+const { scrapeFunds } = require("./funds/scrapeFunds");
+const { scrapePayouts } = require("./payouts/scrapePayouts");
 
 module.exports = {
   scrapeCommonEntities,
   scrapeFunds,
+  scrapeFundTypes,
+  scrapePayouts,
 };
--- src/server.js
+++ src/server.js
@@ -1,24 +1,38 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
 
-const { aum_report_url } = require("./constants");
-const { scrapeCommonEntities, scrapeFunds } = require("./scraper-functions");
+const { aum_report_url, payout_url } = require("./constants");
+const {
+  scrapeCommonEntities,
+  scrapeFunds,
+  scrapeFundTypes,
+} = require("./scraper-functions");
 
 const startScraping = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(aum_report_url);
-  const { fundTypesTable, amcsTable, fundCategoriesTable } =
-    await scrapeCommonEntities({ page, browser });
+  const fundTypes = await scrapeFundTypes({ page });
+  const { amcsTable, fundCategoriesTable } = await scrapeCommonEntities({
+    page,
+    browser,
+    linksToScrape: fundTypes?.map(({ link }) => link),
+  });
   const fundsTable = await scrapeFunds({
     page,
     browser,
-    linksToScrape: fundTypesTable?.map(({ link }) => link),
+    linksToScrape: fundTypes?.map(({ link }) => link),
   });
+
+  // start scraping for payouts
+  await page.goto(payout_url);
+  const payoutFundTypes = await scrapeFundTypes({ page });
+
+  console.log("Payout funds type", payoutFundTypes);
 
   await browser.close();
 
-  await fs.writeFile("fund-types.txt", JSON.stringify(fundTypesTable));
+  await fs.writeFile("fund-types.txt", JSON.stringify(fundTypes));
   await fs.writeFile("amcs.txt", JSON.stringify(amcsTable));
   await fs.writeFile(
     "fund-categories.txt",
